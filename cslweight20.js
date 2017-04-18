dfc94fe867b131e77181f94522b9d6f7510c0451
Reusi <reusinow@gmail.com>
2017-04-17 19:36:35 -0500

    Update cslweight20.js

--- cslweight20.js
+++ cslweight20.js
@@ -1,20 +1,15 @@
 var stripe = require("stripe")("sk_test_A6y8k03vHkuGhVs5wXEVjpjJ");
 
-var token = request.body.stripeToken;
-stripe.customers.create({
-  email: "paying.user@example.com",
-  source: token,
-}).then(function(customer) {
-    
-  return stripe.charges.create({
-    amount: 3000,
-    currency: "usd",
-    customer: customer.id,
-  });
-}).then(function(charge) {
-});
-stripe.charges.create({
-  amount: 3000, 
-  currency: "usd",
-  customer: customerId,
-});
+module.exports = function (ctx, req, res) {
+    stripe(ctx.secrets.stripeSecretKey).charges.create({
+        amount: 3000,
+        currency: 'usd',
+        source: ctx.body.stripeToken,
+        description: 'Laundry Service'
+    }, function (error, charge) {
+        var status = error ? 400 : 200;
+        var message = error ? error.message : 'Thanks for ordering!'; 
+        res.writeHead(status, { 'Content-Type': 'text/html' });
+        return res.end('<h1>' + message + '</h1>');
+    });
+};
